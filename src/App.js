be174759c2122286568418d527c6ea007de29594
dev-ming <ruaaaa@ajou.ac.kr>
2023-08-09 16:12:07 +0900

    :sparkles: Add gpt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const [result, setResult] = useState('');
+
+  const convertText = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/convert', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text })
+      });
+      const data = await response.json();
+      setResult(data.politeText);
+    } catch (error) {
+      console.error('Error converting text:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>hi!</p>
-      </header>
-    </div>
+      <div className="App">
+        <h2>문장을 공손하게 변환하세요</h2>
+        <input
+            type="text"
+            value={text}
+            onChange={e => setText(e.target.value)}
+        />
+        <button onClick={convertText}>변환하기</button>
+        <p>결과: {result}</p>
+      </div>
   );
 }
 
